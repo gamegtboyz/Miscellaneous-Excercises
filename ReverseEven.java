a7fd411c0be7779e908b2fce5869b4f5e6c4bd7c
Asa Bejraputra <102132550+gamegtboyz@users.noreply.github.com>
2022-05-19 18:01:35 +0700

    Update ReverseEven.java

--- ReverseEven.java
+++ ReverseEven.java
@@ -2,15 +2,18 @@ public class ReverseEven {
     public static void main(String[] data) {
         int[] a = {2, 3, 4, 2, 1, 8, 6};
 
+        // build the new array with the same size, and int variable called revrsed index
         int[] rev_a = new int[a.length];
         int rev_index = a.length - 1;
 
         for (int i = 0; i < a.length; i++) {
 
+            // if it's odd, place them on the same index
             if (a[i] % 2 != 0) {
                 rev_a[i] = a[i];
             }
 
+            // if it's even, find the new even from backwards
             if (a[i] % 2 == 0) {
                 if (a[rev_index] % 2 != 0) {
                     rev_index--;
@@ -22,6 +25,7 @@ public class ReverseEven {
                 }
             }
         }
+        // print the new reversed array
         for (int i = 0; i < rev_a.length; i++) {
             System.out.print(rev_a[i] + " ");
         }
