acd7a3ce8b60df1b2a72ef84b8de78240bd6615c
Asa Bejraputra <102132550+gamegtboyz@users.noreply.github.com>
2022-06-11 01:42:16 +0700

    Add files via upload

---
+++ Maxint.java
@@ -0,0 +1,34 @@
+class Maxint {
+    public static void main(String[] args) {
+        int[] input = {3, 30, 34, 5, 9};
+        
+        // create the max value from given integer array
+        // create descending sorted array
+        int temp;
+        for (int i = 1; i < input.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if(input[j] > input[j-1]) {
+                    temp = input[j];
+                    input[j] = input[j-1];
+                    input[j-1] = temp;
+                }
+            }
+        }        
+
+        //convert the sorted int[] onto String
+        String[] k = new String[input.length];
+        for(int i = 0; i < input.length; i++) {
+            k[i] = String.valueOf(input[i]);        
+            //System.out.print(k[i]);
+        }
+        
+        //append String
+        String t = k[0];
+        for(int i = 1; i < input.length; i++) {
+            t += k[i];
+        }
+        //System.out.println(t);
+        int result = Integer.valueOf(t);
+        System.out.println(result);
+    }
+}
